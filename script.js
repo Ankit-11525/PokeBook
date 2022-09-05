12a94a3de0d98cf9a21bec55a9ff857561ba9c4d
ANKIT KUMAR PATHAK <76417084+Ankit-11525@users.noreply.github.com>
2022-09-05 23:01:14 +0530

    Update script.js
    file update js

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
 const search_term=document.getElementById('search_q')
+const search_btn=document.getElementById('search-btn')
+// console.log(search_term)
+// api http://pokeapi.co/docs/v2#pokemon
+const getPokemonData = async term => {
+    // console.log(${term})
+    document.getElementById('show_error').classList.remove('show')
+    document.getElementById('show_error').classList.add('hidden')
+    const url= `https://pokeapi.co/api/v2/pokemon/${term}`
+    const response= await fetch(url)
+    if(response.status==404 || response.statusText=='Not found')
+    {
+        document.getElementById('show_error').classList.add('show')
+        document.getElementById('show_error').classList.remove('hidden')
+        return 
+    }
+    const pokemon =await response.json()
+    debugger
+    //update ui with data
+    document.getElementById('update_img').setAttribute('src',pokemon.sprites.other.dream_world.front_default)
+    document.getElementById('update_name').innerHTML = pokemon.name
+    document.getElementById('update_candy_title').innerHTML = `${pokemon.name} Candy`
+    document.getElementById('update_hp').innerHTML = `HP ${Math.floor((Math.random() * pokemon.stats[0].base_stat) + 1)}/${pokemon.stats[0].base_stat}`
+    document.getElementById('update_cp').innerHTML = `XP ${pokemon.base_experience}`
+    document.getElementById('update_type').innerHTML = `${pokemon.types[0].type.name} / ${pokemon.types[1].type.name}`
+    document.getElementById('update_weight').innerHTML = `${pokemon.weight}kg`
+    document.getElementById('update_height').innerHTML = `0.${pokemon.height}m`
+    document.getElementById('update_stardust').innerHTML = Math.floor((Math.random() * 10000) + 1)
+    document.getElementById('update_candy').innerHTML = Math.floor((Math.random() * 200) + 1)
+    document.getElementById('update_link').setAttribute('href',`https://www.pokemon.com/us/pokedex/${term}`)
+}
+search_btn.addEventListener('click', () => getPokemonData(search_term.value))
+
